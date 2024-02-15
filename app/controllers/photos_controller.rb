85ce503309bc212454767206c4443a046a4e94b2
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2024-02-15 14:28:05 +0000

    Added delete route to delete photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,19 +1,29 @@
 class PhotosController < ApplicationController
   def index
     matching_photos = Photo.all
-    @list_of_photos = matching_photos.order({:created_at => :desc})
-    render({:template => "/photo_templates/index"})
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+    render({ :template => "/photo_templates/index" })
   end
-  # def show
-  #   # Parameters: {"user_id"=>"christina"}
-  #   user_info = params.fetch("user_id")
-  #   matching_users = User.where({:username => user_info})
-  #   @the_user = matching_users[0]
 
-  #   if @the_user == nil
-  #     redirect_to("/404")
-  #   else 
-  #     render({:template => "user_templates/show"})
-  #   end
-  # end
+  def show
+    photo_info = params.fetch("photo_id")
+    matching_photos = Photo.where({ :id => photo_info })
+    @the_photo = matching_photos[0]
+
+    if @the_photo == nil
+      redirect_to("/404")
+    else
+      render({ :template => "photo_templates/show" })
+    end
+  end
+
+  def delete
+    photo_id = params.fetch("photo_id")
+    matching_photos = Photo.where({ :id => photo_id })
+    the_photo = matching_photos[0]
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:user_id", {:controller => "users", :action => "show"})
+
   get("/photos", {:controller => "photos", :action => "index"})
+  get("/photos/:photo_id", {:controller => "photos", :action => "show"})
+
+
+  get("/delete_photo/:photo_id", {:controller => "photos", :action => "delete"})
+
+  # post("/add_comment", {:controller => "comments", :action => "add_comment"} )
 end
