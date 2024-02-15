7d93e361bfeb74b0d206a3cb31dc6f3d0459d0d7
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2024-02-15 18:14:33 +0000

    User can now create photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,6 +6,7 @@ class PhotosController < ApplicationController
   end
 
   def show
+    # Parameters: {"image"=>"lala", "caption"=>"lala", "owner_id"=>"113", "photo_id"=>"show"}
     photo_info = params.fetch("photo_id")
     matching_photos = Photo.where({ :id => photo_info })
     @the_photo = matching_photos[0]
@@ -25,5 +26,28 @@ class PhotosController < ApplicationController
     the_photo.destroy
 
     redirect_to("/photos")
+  end
+
+  def add
+    # Parameters: {"image"=>"https://www.theodinproject.com/assets/icons/odin-icon-a34029cd84a741be4da27758dafd7d7ac3729021adea3e0701e310d91e5c1d04.svg", "caption"=>"Odin", "owner_id"=>"115"}
+    new_photo = Photo.new
+    new_photo.image = params.fetch("image")
+    new_photo.caption = params.fetch("caption")
+    new_photo.owner_id = params.fetch("owner_id")
+
+    new_photo.save 
+
+    redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
+  def add_comment
+    new_comment = Comment.new
+    new_comment.photo_id = params.fetch("input_photo_id")
+    new_comment.author_id = params.fetch("input_author_id")
+    new_comment.body = params.fetch("input_comment")
+
+    new_comment.save
+
+    redirect_to("/photos")
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "users", :action => "index"})
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:user_id", {:controller => "users", :action => "show"})
 
   get("/photos", {:controller => "photos", :action => "index"})
   get("/photos/:photo_id", {:controller => "photos", :action => "show"})
-
-
   get("/delete_photo/:photo_id", {:controller => "photos", :action => "delete"})
-
-  # post("/add_comment", {:controller => "comments", :action => "add_comment"} )
+  post("/add_photo", {:controller => "photos", :action => "add"})
+  post("/add_comment", {:controller => "photos", :action => "add_comment"} )
 end
